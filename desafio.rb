d69e127843f0f72e79aa4878e96ca8bdc1ca866b
Kevin Heidorn <128178416+Kevin-Heidorn@users.noreply.github.com>
2024-04-11 19:07:03 -0300

    continues

---
+++ desafio.rb
@@ -0,0 +1,10 @@
+puts ("Digite seu nome :")
+nome = gets.chomp
+
+puts("DIGITE SEU SOBRENOME")
+sobrenome = gets.chomp
+
+puts("Agora sua idade :")
+idade = gets.chomp.to_i
+
+puts("O nome do nosso participante é #{nome} #{sobrenome} com #{idade} anos ")
---
+++ logica.rb
@@ -0,0 +1,21 @@
+#EXERCICIOS EM RUBY(entrada e saida de dados ) 
+#puts ("Digite um numero : ")
+#n1 = gets.chomp.to_i
+#puts ("Digite outro numero : ")
+#n2 = gets.chomp.to_i 
+#puts ("Digite o ultimo numero :")
+#n3 = gets.chomp.to_i
+#s = (n1+n2+n3)/3
+#puts ("A media de todos os termos é #{s}")
+
+#celcius for farheit
+#puts ("Digite qual a temperatura em graus :")
+#temperature = gets.chomp.to_f
+#farheit = (temperature*9/5)+32
+#puts ("A TEMPERATURA EM FARHEIT É #{farheit}")
+#area circulo
+puts ("Qual é o raio do circulo : ")
+thunder = gets.chomp.to_i 
+circle = (3.14*thunder**2)
+puts(circle)
+
