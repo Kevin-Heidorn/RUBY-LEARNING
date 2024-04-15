9f553bab4332f9c5bc9d68666a8cc15464ac0562
Kevin Heidorn <128178416+Kevin-Heidorn@users.noreply.github.com>
2024-04-15 09:36:46 -0300

    if,else and elsif

---
+++ if.rb
@@ -0,0 +1,30 @@
+#Condições ruby
+#just if 
+ #dia = "domingo"
+#if (dia == "domingo") 
+ #almoco ='especial'
+#end  
+#puts  "Hojo nosso almoço será #{almoco}"
+#else
+#dia = "terça"
+#if (dia == "domingo") 
+ #almoco ='especial'
+#else 
+ # almoco= 'normal '
+#end 
+ #puts  "Hojo nosso almoço será #{almoco}"
+ #elseif
+ dia = "feriado"
+
+if (dia == "domingo"); 
+
+ almoco ='especial'
+
+ elsif (dia == "feriado");
+    almoco = "mais tarde"
+
+else 
+  almoco = 'normal'  
+
+end 
+ puts  "Hojo nosso almoço será #{almoco}"
