25c6419d9a78cdd763e8c866861e0a5bdc663ea3
Kevin Heidorn <128178416+Kevin-Heidorn@users.noreply.github.com>
2024-04-24 20:41:29 -0300

    att cases and for

---
+++ Aulasruby/case.rb
@@ -0,0 +1,22 @@
+#Aprendendo sobre Case
+puts ('digite o seu mes de nascimento: ')
+age = gets.chomp.to_i
+#definindo casos
+case age
+ when 1..3 #intervalo é utilizado quando usa (x..y)
+    puts ("You born in  first trimester")   
+
+ when 4..6
+    puts ("You born in second trimester ") 
+
+ when 7..9
+    puts("You born in third trimester ") 
+
+ when 10..12
+   puts("you born in the last of year")
+
+ else  
+    puts("Do you think life is easy? ")
+
+   end
+
---
+++ Aulasruby/for.rb
@@ -0,0 +1,8 @@
+#Array listas de elementos 
+languages = ['ruby','python','java','javascript']
+count = 1
+for language in languages
+    puts (language)
+    puts (count)
+    count = count + 1
+end
