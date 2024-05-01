56b4ce4f11a6457111e36c1bfcc5d4200e4c3815
Kevin-Heidorn <kevinheidorn20@gmail.com>
2024-05-01 10:57:17 -0300

    git

---
+++ Aulasruby/time.rb
@@ -0,0 +1,21 @@
+#Exemplo puts 1
+#6.times {puts("olá, Mundo! ")}
+
+#Exemplo print 1
+#6.times {print("olá, Mundo! ")}
+
+#Exemplo puts 2
+#4.times do 
+#    puts "Oi"
+# end
+
+
+#Exemplo print 2
+#4.times do 
+  #  print "Oi"
+#end
+20.times do |contador|
+ puts "#{contador}"
+end
+
+
